40d6d9a1791bc05c4efeddd7c53a72b82313b3f4
Carlosgb22 <carlitosgebur@gmail.com>
2023-02-17 08:08:44 +0100

    Añadir logger para mantener un control

--- src/contexts/device/infrastructure/dataSources/mysql/device.mysql.adapter.ts
+++ src/contexts/device/infrastructure/dataSources/mysql/device.mysql.adapter.ts
@@ -6,7 +6,7 @@ import loggerService from "../../../../../services/loggerService";
 export default class DeviceMySQL implements database {
 conn = connect()
     async getAll(): Promise<Array<Device>> {
-        
+        loggerService.info("Obteniendo dispositivos...")
         return new Promise<Array<Device>>(async (resolve, reject) => {
             (await this.conn).query("SELECT * FROM devices", function (error, results, fields) {
                 if (error) loggerService.error(error);
@@ -16,7 +16,7 @@ conn = connect()
     }
 
     async getDeviceById(id: string): Promise<Device> {
- 
+        loggerService.info("Obteniendo dispositivo por su id...")
         return new Promise<Device>(async (resolve, reject) => {
             (await this.conn).query("SELECT * FROM devices WHERE id = " + (await this.conn).escape(id), function (error, results, fields) {
                 if (error) loggerService.error(error);
@@ -26,7 +26,7 @@ conn = connect()
     }
 
     async addDevice(dev: Device): Promise<boolean> {
-
+        loggerService.info("Añadiendo dispositivo...")
         return new Promise<boolean>(async (resolve, reject) => {
             (await this.conn).query("INSERT INTO devices VALUES (?, ?, ?, ?, ?, ?)", [dev.id, dev.name, dev.userUid, dev.imgcon, dev.imgdiscon, dev.imgwait],
                 function (error, results, fields) {
@@ -37,6 +37,7 @@ conn = connect()
     }
 
     async deleteDevice(id: String): Promise<boolean> {
+        loggerService.info("Borrando dispositivo...")
         return new Promise<boolean>(async (resolve, reject) => {
             (await this.conn).query("DELETE FROM devices WHERE id = " + (await this.conn).escape(id), function (error, results, fields) {
                 if (error) loggerService.error(error);
@@ -48,6 +49,7 @@ conn = connect()
     }
 
     async updateDevice(dev: Device): Promise<boolean> {
+        loggerService.info("Actualizando dispositivo...")
         return new Promise<boolean>(async (resolve, reject) => {
             (await this.conn).query("UPDATE devices SET name = ?, userUid = ?, imgcon = ?, imgdiscon = ?, imgwait = ? WHERE id = ?",
                 [dev.name, dev.userUid, dev.imgcon, dev.imgdiscon, dev.imgwait, dev.id], function (error, results, fields) {
